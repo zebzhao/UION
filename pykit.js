31e2197d911da95a5c5203dc2603abc4431d1a1f
zebzhao <zebzhao@users.noreply.github.com>
2016-02-14 13:08:44 -0500

    fix ReferenceError with setActive

--- pykit.js
+++ pykit.js
@@ -1875,9 +1875,12 @@ pykit.UI.list = pykit.defUI({
 			this.dispatch("onItemSelectionChanged", [item]);
 	},
 	setActiveLabel: function(label) {
+		this.setActive("label", label)
+	},
+	setActive: function(key, value) {
 		this.unselectAll();
-		var item = this.findOne("label", label);
-		pykit.assert(item, pykit.replaceString("Could not find label {label} in {id}.", {label: label, id: this.id}));
+		var item = this.findOne(key, value);
+		pykit.assert(item, pykit.replaceString("Could not find {key} {value} in {id}.", {key: key, value: value, id: this.id}));
 		this.select(item);
 	},
 	isSelected: function(target) {
