3935e7f8cade00dad43b1896c08e3ea24a69d479
zeb <zeb.zhao@gamecheetah.net>
2016-05-17 23:27:39 -0400

    master now refers to master ui not config

--- pykit.debug.js
+++ pykit.debug.js
@@ -5588,7 +5588,8 @@ pykit.UI.element = pykit.defUI({
 			return value;
 		},
 		dropdown: function(value) {
-			var config = this._config;
+			var $this = this;
+			var config = $this._config;
 
 			var dropdown = {
 				view: "dropdown",
@@ -5601,9 +5602,9 @@ pykit.UI.element = pykit.defUI({
 
 			this._config.on = config.on || {};
 			this.addListener(config.dropdownEvent, function(config, node, e) {
-				ui.open(node, config, e);
+				ui.open(node, $this, e);
 			});
-			this.dropdownPopup = ui;
+			$this.dropdownPopup = ui;
 			return value;
 		},
 		inline: function(value) {
@@ -5805,12 +5806,12 @@ pykit.UI.flexgrid = pykit.defUI({
 		this.dispatch("onChildChange",[this._activeChild, newChild]);
 		this._activeChild = newChild;
 	},
-	showBatch:function(name, rerender){
+	showBatch:function(name, preserveOrder){
 		/**
 		 * Tricky: Rendering input fields will cause problems with on-screen keyboards.
 		 * However, to preserve the order of elements, will need to rerender.
 		 */
-		this._setVisible('batch', name, rerender);
+		this._setVisible('batch', name, preserveOrder);
 		this.batch = name;
 	},
 	_setVisible: function(key, value, rerender) {
